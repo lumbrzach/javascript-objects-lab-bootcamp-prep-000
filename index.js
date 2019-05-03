b372e030109934caac985f5e46ebf2312d9637c9
Zach Parker <parkerz410@gmail.com>
2019-05-03 16:10:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},object,{[key]: value})
-}
+};
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign(obj, { [key]: value };
+};
+
+function deleteFromObjectByKey(object, key){
+  return delete object['key'];
+};
